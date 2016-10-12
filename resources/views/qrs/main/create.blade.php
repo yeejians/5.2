f95504260259909a0d23d4b42c66926d94b4a2b0
yeejians <yeejians@gmail.com>
2016-10-12 16:58:24 +0800

    Change layout to multiple SO input

--- resources/views/qrs/main/create.blade.php
+++ resources/views/qrs/main/create.blade.php
@@ -57,158 +57,10 @@ QR List :: Create ::
 			</div>
 		</div>
 
-		<div class="form-group {{ $errors->has('sono') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="sono">Sales Order No.:</label>
-			<div class="col-md-3">
-				<div class="input-group">
-					<input type="text" class="form-control" name="sono" id="sono" value="{{ old('sono') }}" required />
-					<span class="input-group-btn">
-						<button type="button" class="btn btn-success check-txt" for="sono">Check</button>
-					</span>
-				</div>	
-			</div>
-		</div>
-
 		<div class="form-group {{ $errors->has('customer_name') ? 'has-error' : '' }}">
 			<label class="col-md-3 col-md-fixed control-label" for="customer_name">Customer Name:</label>
 			<div class="col-md-6">
 				<input type="text" class="form-control" name="customer_name" id="customer_name" value="{{ old('customer_name') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('customer_id') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="customer_id">Customer ID:</label>
-			<div class="col-md-2">
-				<input type="text" class="form-control" name="customer_id" id="customer_id" value="{{ old('customer_id') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('country_id') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="country_id">Customer Country:</label>
-			<div class="col-md-3">
-				<select class="form-control" name="country_id" id="country_id" required>
-					<option></option>
-					@foreach ($countries as $country)
-						<option value="{{ $country->id }}"{!! (old('country_id') == $country->id ? ' selected="selected"' : '') !!}>{{ $country->name }}</option>
-					@endforeach
-				</select>
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('batch') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="batch">Batch No.:</label>
-			<div class="col-md-3">
-				<input type="text" class="form-control" name="batch" id="batch" value="{{ old('batch') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('outbound') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="outbound">Outbound No.:</label>
-			<div class="col-md-3">
-				<input type="text" class="form-control" name="outbound" id="outbound" value="{{ old('outbound') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('production_at') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="production_at">Production Date:</label>
-			<div class="col-md-6">
-				<input type="text" class="form-control" name="production_at" id="production_at" value="{{ old('production_at') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('dispatch_at') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="dispatch_at">Dispatch Date:</label>
-			<div class="col-md-6">
-				<input type="text" class="form-control" name="dispatch_at" id="dispatch_at" value="{{ old('dispatch_at') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('arrival_at') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="arrival_at">Arrival Date:</label>
-			<div class="col-md-6">
-				<input type="text" class="form-control" name="arrival_at" id="arrival_at" value="{{ old('arrival_at') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('incoterm') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="incoterm">Incoterm:</label>
-			<div class="col-md-3">
-				<input type="text" class="form-control" name="incoterm" id="incoterm" value="{{ old('incoterm') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('product_caiscode') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="product_caiscode">Product CAIS Code:</label>
-			<div class="col-md-3">
-				<input type="text" class="form-control" name="product_caiscode" id="product_caiscode" value="{{ old('product_caiscode') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('product_name') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="product_name">Product Name:</label>
-			<div class="col-md-6">
-				<input type="text" class="form-control" name="product_name" id="product_name" value="{{ old('product_name') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('product_category') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="product_category">Product Category:</label>
-			<div class="col-md-9">
-				<label class="radio-inline"><input type="radio" name="product_category" id="product_category_1" value="Commodity"{!! (old('product_category') == 'Commodity' ? ' checked="checked"' : '') !!} /> Commodity</label>
-				<label class="radio-inline"><input type="radio" name="product_category" id="product_category_0" value="Specialty"{!! (old('product_category') == 'Specialty' ? ' checked="checked"' : '') !!} /> Specialty</label>
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('container') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="container">Container No.:</label>
-			<div class="col-md-3">
-				<input type="text" class="form-control" name="container" id="container" value="{{ old('container') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('reefer') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="reefer">Reefer Container:</label>
-			<div class="col-md-9">
-				<label class="radio-inline"><input type="radio" name="reefer" id="reefer_1" value="1"{!! (old('reefer') == 1 ? ' checked="checked"' : '') !!} /> Yes</label>
-				<label class="radio-inline"><input type="radio" name="reefer" id="reefer_0" value="0"{!! (old('reefer') == 0 ? ' checked="checked"' : '') !!} /> No</label>
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('quantity') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="quantity">Affected Quantity (MT):</label>
-			<div class="col-md-2">
-				<input type="text" class="form-control" name="quantity" id="quantity" value="{{ old('quantity') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('pack_id') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="pack_id">Packing Type:</label>
-			<div class="col-md-3">
-				<select class="form-control" name="pack_id" id="pack_id" required>
-					<option></option>
-					@foreach ($packing as $pack)
-						<option value="{{ $pack->id }}"{!! (old('pack_id') == $pack->id ? ' selected="selected"' : '') !!}>{{ $pack->name }}</option>
-					@endforeach
-				</select>
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('size') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="size">Packing Size (KG):</label>
-			<div class="col-md-2">
-				<input type="text" class="form-control" name="size" id="size" value="{{ old('size') }}" required />
-			</div>
-		</div>
-
-		<div class="form-group {{ $errors->has('stuff_id') ? 'has-error' : '' }}">
-			<label class="col-md-3 col-md-fixed control-label" for="stuff_id">Stuffing Type:</label>
-			<div class="col-md-3">
-				<select class="form-control" name="stuff_id" id="stuff_id" required>
-					<option></option>
-					@foreach ($stuffing as $stuff)
-						<option value="{{ $stuff->id }}"{!! (old('stuff_id') == $stuff->id ? ' selected="selected"' : '') !!}>{{ $stuff->name }}</option>
-					@endforeach
-				</select>
 			</div>
 		</div>
 
@@ -258,8 +110,6 @@ QR List :: Create ::
 			$('#saleslist').select2();
 
 			$('#country_id').select2({placeholder: "-- Please Select --"});
-			$('#stuff_id').select2({placeholder: "-- Please Select --"});
-			$('#pack_id').select2({placeholder: "-- Please Select --"});
 
 			$('#site_id').select2();
 			$('#site_id').select2("readonly", true);
@@ -280,12 +130,6 @@ QR List :: Create ::
 				},
 				messages: {
 					country_id: {
-						required: "this field is required"
-					},
-					pack_id: {
-						required: "this field is required"
-					},
-					stuff_id: {
 						required: "this field is required"
 					}
 				}
